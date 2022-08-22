8090c462597f531a3d6ce61aebecf3c63b48c900
Sabrina Powell <39811028+Sabby12@users.noreply.github.com>
2022-08-22 10:00:41 -0500

    TODO 3: Classify Images

--- classify_images.py
+++ classify_images.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
-# REVISED DATE: 
+# PROGRAMMER: Sabrina Powell
+# DATE CREATED: August 22, 2022                            
+# REVISED DATE: August 22, 2022
 # PURPOSE: Create a function classify_images that uses the classifier function 
 #          to create the classifier labels and then compares the classifier 
 #          labels to the pet image labels. This function inputs:
@@ -23,12 +23,6 @@
 # Imports classifier function for using CNN to classify images 
 from classifier import classifier 
 
-# TODO 3: Define classify_images function below, specifically replace the None
-#       below by the function definition of the classify_images function. 
-#       Notice that this function doesn't return anything because the 
-#       results_dic dictionary that is passed into the function is a mutable 
-#       data type so no return is needed.
-# 
 def classify_images(images_dir, results_dic, model):
     """
     Creates classifier labels with classifier function, compares pet labels to 
@@ -65,4 +59,19 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    None 
+    # Processes each file in the results dictionary
+    for key in results_dic:
+      # Classifies image
+      model_label = classifier(images_dir + key, model)
+      # Formats image for comparison
+      model_label.lower().strip()
+
+      # Retrieve the actual label from the results dictionary
+      true_label = results_dic[key][0]
+
+      # Extends results dictionary with the label returned by the model 
+      # and whether the model correctly classified the image
+      if true_label in model_label:
+        results_dic[key].extend([model_label, 1])
+      else:
+        results_dic[key].extend([model_label, 0])
