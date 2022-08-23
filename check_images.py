202f30c12b82698d48b8e874e84047bae056366a
Sabrina Powell <39811028+Sabby12@users.noreply.github.com>
2022-08-23 01:33:58 -0500

    TODO 6: Display Results

--- check_images.py
+++ check_images.py
@@ -68,23 +68,17 @@ def main():
 
     # Function that checks Results Dictionary for is-a-dog adjustment using results
     check_classifying_labels_as_dogs(results)
-       
+
     # Calculates results of run and puts statistics in the Results Statistics
     # Dictionary - called results_stats
     results_stats = calculates_results_stats(results)
 
     # Function that checks Results Statistics Dictionary using results_stats
     check_calculating_results(results, results_stats)
-
-
-    # TODO 6: Define print_results function within the file print_results.py
-    # Once the print_results function has been defined replace 'None' 
-    # in the function call with in_arg.arch  Once you have done the 
-    # replacements your function call should look like this: 
-    #      print_results(results, results_stats, in_arg.arch, True, True)
+    
     # Prints summary results, incorrect classifications of dogs (if requested)
     # and incorrectly classified breeds (if requested)
-    print_results(results, results_stats, None, True, True)
+    print_results(results, results_stats, in_arg.arch, True, True)
     
     end_time = time()
     
